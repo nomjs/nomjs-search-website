d57dae30ae14a7ee93dc9186ba9034a2a1ab0cd6
Daniela Baron <baron.daniela@gmail.com>
2016-04-03 18:08:33 -0400

    a quick test to verify proxy is working

--- src/welcome.js
+++ src/welcome.js
@@ -1,16 +1,18 @@
-//import {computedFrom} from 'aurelia-framework';
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
 
+@inject(HttpClient)
 export class Welcome {
   heading = 'Welcome to the nomjs!';
   firstName = 'John';
   lastName = 'Doe';
   previousValue = this.fullName;
 
-  //Getters can't be directly observed, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below
-  //as well as the corresponding import above.
-  //@computedFrom('firstName', 'lastName')
+  constructor(http) {
+    this.http = http;
+  }
+
   get fullName() {
     return `${this.firstName} ${this.lastName}`;
   }
@@ -24,6 +26,13 @@ export class Welcome {
     if (this.fullName !== this.previousValue) {
       return confirm('Are you sure you want to leave?');
     }
+  }
+
+  // verify that api proxy is working
+  activate() {
+    return this.http.fetch('/api')
+      .then(response => response.json())
+      .catch(err => console.log(err));
   }
 }
 
