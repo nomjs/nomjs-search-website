fc391317b74bd624c1bd864e5ad59012c848de8f
Daniela Baron <baron.daniela@gmail.com>
2016-04-10 16:18:15 -0400

    sanitize html for search suggestions

--- src/search/search-bar.js
+++ src/search/search-bar.js
@@ -1,6 +1,7 @@
 import {inject} from 'aurelia-framework';
 import $ from 'jquery';
 import 'devbridge-autocomplete';
+import sanitizeHtml from 'sanitize-html';
 
 // Autocomplete provided by https://github.com/devbridge/jQuery-Autocomplete
 @inject(Element)
@@ -11,9 +12,9 @@ export class SearchBarCustomElement {
 
   attached() {
     $('#packageSearch').autocomplete({
-      onSelect: this.onSelect.bind(this),
       lookup: this.localPackages(),   // TODO replace with ajax serviceUrl
-      formatResult: this.formatResult.bind(this)
+      formatResult: this.formatResult.bind(this),
+      onSelect: this.onSelect.bind(this)
     });
   }
 
@@ -22,18 +23,24 @@ export class SearchBarCustomElement {
     return [
       { value: '@aaa/aaa', data: { scope: '@aaa', name: 'aaa', description: 'The aaa module'} },
       { value: '@aaa/foo', data: { scope: '@aaa', name: 'foo', description: 'The foo module'} },
-      { value: '@bbb/foo', data: { scope: '@bbb', name: 'foo', description: 'Look at that, bbb also has a foo'} }
+      { value: '@bbb/foo', data: { scope: '@bbb', name: 'foo', description: 'Look at that, bbb also has a foo'} },
+      { value: '@john/aaa-api', data: { scope: '@john', name: 'aaa-api', description: 'This has some html <br> that should be <a href="http://go-there">sanitized</a>'} }
     ];
+  }
+
+  // TODO Move sanitizing to suggestion service
+  formatResult(suggestion, currentValue) {
+    let sanitizeOpts = {
+      allowedTags: [],
+      allowedAttributes: []
+    };
+    let line1 = sanitizeHtml(suggestion.value, sanitizeOpts);
+    let line2 = sanitizeHtml(suggestion.data.description, sanitizeOpts);
+    return `${line1} <br> ${line2}`;
   }
 
   // Future: Navigate to package detail page
   onSelect(suggestion) {
-    console.log(`you suggested ${suggestion.value}, ${suggestion.data}`);
-  }
-
-  // TODO Can aurelia templating be used and does it sanitize?
-  // http://ilikekillnerds.com/2016/01/enhancing-at-will-using-aurelias-templating-engine-enhance-api/
-  formatResult(suggestion, currentValue) {
-    return `${suggestion.value} <br> ${suggestion.data.description}`;
+    // console.log(`you suggested ${suggestion.value}, ${suggestion.data}`);
   }
 }
