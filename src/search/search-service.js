fff3ece8aab2f54189d91b57cf700978866d7437
Daniela Baron <baron.daniela@gmail.com>
2016-04-16 08:15:57 -0400

    #4 introduce a sample registry search response and a simple searchservice

---
+++ src/search/search-service.js
@@ -0,0 +1,16 @@
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
+
+@inject(HttpClient)
+export class SearchService {
+  constructor(http) {
+    this.http = http;
+  }
+
+  // TODO transform response for autocomplete before returning
+  searchPackage() {
+    return this.http.fetch('/sample-data/search.json')
+      .then(response => response.json());
+  }
+}
