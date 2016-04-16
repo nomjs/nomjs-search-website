004db0f3cfd9f36d4d5b16a8c901ceb9bac9f77a
Daniela Baron <baron.daniela@gmail.com>
2016-04-16 17:33:19 -0400

    verify suggestions are sanitized

--- test/unit/search/suggestion-service.spec.js
+++ test/unit/search/suggestion-service.spec.js
@@ -6,11 +6,23 @@ describe('SuggestionService', () => {
     suggestionService = new SuggestionService();
   });
   describe('formatSuggestion', () => {
-    it('Formats a suggestion for auto copmlete', () => {
+    it('Formats a suggestion for auto complete', () => {
       // Given
       let suggestion = {
         value: 'foo',
         data: {name: 'foo', description: 'the foo package'}
+      };
+      let currentValue = 'fo';
+      // When
+      let result = suggestionService.formatSuggestion(suggestion, currentValue);
+      // Then
+      expect(result).toEqual('<strong>fo</strong>o<div class="suggestion-sub">the foo package</div>');
+    });
+    it('Removes HTML from description', () => {
+      // Given
+      let suggestion = {
+        value: 'foo',
+        data: {name: 'foo', description: 'the <h1>foo</h1> package'}
       };
       let currentValue = 'fo';
       // When
