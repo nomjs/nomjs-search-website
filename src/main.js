a2a6829407ec8ca2194c4ec98b5e668e7ce09a4b
Daniela Baron <baron.daniela@gmail.com>
2016-05-17 17:51:41 -0400

    #8 proof of concept that http fetch can be called prior to app startup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// import {HttpClient} from 'aurelia-fetch-client';
+
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
@@ -10,5 +12,14 @@ export function configure(aurelia) {
   //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   //aurelia.use.plugin('aurelia-html-import-template-loader')
 
+  // TODO After config endpoint is available in nom-registry, load it here before app startup
+  // It will have information like github oauth url
+  // Proof of concept to verify can make http call before app startup
+  // let httpClient = new HttpClient();
+  // httpClient.fetch('https://api.github.com/users')
+  //   .then(response => response.json())
+  //   .then(users => {
+  //     aurelia.start().then(() => aurelia.setRoot());
+  //   });
   aurelia.start().then(() => aurelia.setRoot());
 }
