7d8225015c22f0ac51d07192d986de6c3441f6a3
Daniela Baron <dbaron@uncharted.software>
2016-06-03 08:06:22 -0400

    #12 resolve with github username instead of id from profile

--- server/src/modules/authconfig.js
+++ server/src/modules/authconfig.js
@@ -4,33 +4,26 @@ const Ravel = require('ravel');
 const Module = Ravel.Module;
 const authconfig = Module.authconfig;
 
-const USER_KEY = 'userid';
-
 @authconfig
 class AuthConfig extends Module {
+
+  // TODO: userId is actually username
+  // hit Github Public API to get profile https://api.github.com/users/:username
   getUserById(userId) {
     this.log.debug(`getUserById: ${userId}`);
-
-    // If we end up storing profile object in redis, then don't need to hit githuh API here?
-    // TODO hit redis to get access token, then hit github API or redis to get profile object.
-    // TODO promisify redis client in ravel?
-    this.kvstore.get(`${USER_KEY}-${userId}`, (err, response) => {
-      if (err) {
-        this.log.error(err);
-      } else {
-        this.log.debug(`redis success for ${userId}: ${response}`);
-      }
-    });
-
     return Promise.resolve({id: userId, username: 'Ghnuberath'});
   }
 
+  /**
+   * Resolve with username as "id" because that is what Github uses to lookup users.
+   */
   verifyCredentials(accessToken, refreshToken, profile) {
-    // this.log.debug(`verifyCredentials: ${accessToken}, ${refreshToken}, ${JSON.stringify(profile)}`);
-    // should we be storing profile in redis or just accessToken and userId?
-    let payload = {accessToken, profile};
-    this.kvstore.set(`${USER_KEY}-${profile.id}`, JSON.stringify(payload));
-    return Promise.resolve(profile);
+    this.log.debug(`verifyCredentials: ${accessToken}, ${refreshToken}, ${JSON.stringify(profile)}`);
+    return Promise.resolve({
+      id: profile.username,
+      accessToken: accessToken,
+      githubProfile: profile
+    });
   }
 }
 
